83904e9b436fbc4565ce50e130b10d4f18a7af29
Dnae <dnae.larsson@gmail.com>
2018-06-07 07:55:34 +0200

    Test commit

--- VillaNova/VillaNova.cs
+++ VillaNova/VillaNova.cs
@@ -38,6 +38,7 @@ namespace VillaNova
 
         public void MovePlayer(int x, int y)
         {
+            // NEW TEST COMMENT
             this.px = px + x;
             this.py = py + y;
         }
